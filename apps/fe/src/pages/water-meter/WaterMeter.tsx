575f8cb992e57573c8c4437b396777f24f20b694
Tien Tran <66483590+TrNgTien@users.noreply.github.com>
2024-04-11 15:38:31 +0700

    Feat/crop image (#12)
    * adjust: adjust build script
    
    * fix: fix typography
    
    * add: add capture screen
    
    * add: add capture screen

--- apps/fe/src/pages/water-meter/WaterMeter.tsx
+++ apps/fe/src/pages/water-meter/WaterMeter.tsx
@@ -1,22 +1,29 @@
 import { IconWrapper, PageHeader } from '@/components';
 import { Color, Style } from '@/theme';
 import clsx from 'clsx';
-import { useRef, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import { CiImageOn as UploadIcon } from 'react-icons/ci';
+import { FiRefreshCcw as FlipIcon } from 'react-icons/fi';
 import { IoCameraOutline as Camera } from 'react-icons/io5';
 import 'react-image-crop/dist/ReactCrop.css';
 import Webcam from 'react-webcam';
 
-console.log('checking ', window.innerHeight * 10);
-const videoConstraints = {
-  width: 1280,
-  height: window.innerHeight * 10,
-  facingMode: 'environment',
-};
-
 const WaterMeter = () => {
   const webcamRef = useRef<any>(null);
-  const [imgSrc, setImgSrc] = useState(null);
+  // const [imgSrc, setImgSrc] = useState(null);
+  const [isFrontCamera, setIsFrontCamera] = useState<boolean>(true);
+
+  const videoConstraints = useMemo(() => {
+    return {
+      width: 1280,
+      height: window.innerHeight * 10,
+      facingMode: isFrontCamera ? 'user' : 'environment',
+    };
+  }, [isFrontCamera]);
+
+  const handleFlipCamera = useCallback(() => {
+    setIsFrontCamera((prev) => !prev);
+  }, []);
 
   // function onSelectFile(e: React.ChangeEvent<HTMLInputElement>) {
   //   if (e.target.files && e.target.files.length > 0) {
@@ -43,41 +50,31 @@ const WaterMeter = () => {
         screenshotFormat="image/jpeg"
         videoConstraints={videoConstraints}
       />
-
-      <div
-        className={clsx(Style.FLEX_AROUND, 'mt-4')}>
-        <div className={clsx(Style.FLEX_COL_CENTER)}>
-          <IconWrapper size={28}>
+      <div className={clsx(Style.FLEX_BETWEEN, 'mt-4 p-4')}>
+        <label className={clsx(Style.FLEX_COL_CENTER)} htmlFor="upload-file">
+          <IconWrapper size={24}>
             <UploadIcon />
           </IconWrapper>
-          <p>Tai anh len</p>
           <input type="file" name="upload-file" id="upload-file" hidden />
+        </label>
+        <div className="rounded-full bg-black-900 p-4 border-4 border-white-20">
+          <Camera color={Color.MAIN_WHITE} size={24} className="size-full" />
         </div>
-        <div className="rounded-full bg-black-900 p-6  xs:p-4 top-[-2rem] border-4 border-white-20">
-          <IconWrapper size={24} color={Color.MAIN_WHITE}>
-            <Camera />
+        <div className={clsx(Style.FLEX_COL_CENTER)} onClick={handleFlipCamera}>
+          <IconWrapper size={24}>
+            <FlipIcon />
           </IconWrapper>
         </div>
+        <input
+          type="file"
+          accept="image/png, image/gif, image/jpeg"
+          name="upload-file"
+          id="upload-file"
+          hidden
+        />
       </div>
     </div>
   );
 };
 
 export default WaterMeter;
-
-// <ReactCrop
-//   locked
-//   onDragStart={() => {}}
-//   onDragEnd={() => {}}
-//   crop={crop}
-//   onChange={(c) => setCrop(c)}>
-//   <Webcam
-//     audio={false}
-//     height={720}
-//     ref={webcamRef}
-//     screenshotFormat="image/jpeg"
-//     width={1280}
-//     videoConstraints={videoConstraints}
-//   />
-// </ReactCrop>
-// <button onClick={capture}>Capture photo</button>
