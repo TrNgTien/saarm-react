916be8dda0dc8ef94733c138a933cd96973b6bec
TrNgTien <ngoctien27122001@gmail.com>
2024-04-19 18:39:45 +0700

    adjust: adjust loading component

--- apps/fe/src/components/loading/CommonLoading.tsx
+++ apps/fe/src/components/loading/CommonLoading.tsx
@@ -7,5 +7,15 @@ interface ICommonLoadingProps {
 
 export const CommonLoading = (props: ICommonLoadingProps) => {
   const { color = Color.PRIMARY } = props;
-  return <HashLoader color={color} />;
+  return (
+    <div className="z-50 bg-black-100 opacity-65 fixed top-0 right-0 left-0 bottom-0">
+      <div className="relative top-[40%]">
+        <HashLoader
+          className="absolute left-[38%]"
+          size={100}
+          color={color}
+        />
+      </div>
+    </div>
+  );
 };
