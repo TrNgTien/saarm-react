f494ab08cecca90ecb8551d44552105dd6ef44e4
TrNgTien <ngoctien27122001@gmail.com>
2024-06-11 17:51:05 +0700

    add: add skeleton for image placeholder

--- src/pages/home/Mobile/home-owner/components/HomeCard.tsx
+++ src/pages/home/Mobile/home-owner/components/HomeCard.tsx
@@ -1,15 +1,21 @@
 import Apartment from '@/assets/icons/apartment-placeholder.svg';
 import { IApartment } from '@/common';
 import { RoutePath } from '@/common/constants';
+import { Skeleton } from '@/components/ui/skeleton';
 import { cn } from '@/lib/utils';
 import { Styles } from '@/theme';
+import { useCallback, useState } from 'react';
 import { RiDoorOpenFill as RoomIcon } from 'react-icons/ri';
 import { useNavigate } from 'react-router-dom';
 
 export const HomeCard = (props: IApartment) => {
   const { name, address, totalRoom, roomAvailable, id } = props;
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
 
+  const handleImageLoaded = useCallback(() => {
+    setIsLoading(false);
+  }, []);
   return (
     <div
       className={cn(
@@ -18,7 +24,12 @@ export const HomeCard = (props: IApartment) => {
       )}
       onClick={() => navigate(`${RoutePath.APARTMENT}/${id}`)}>
       <div className="rounded-lg">
-        <img src={Apartment} />
+        {isLoading && (
+          <div className="h-[80px]">
+            <Skeleton className="h-full rounded-xl w-full bg-gray-300" />
+          </div>
+        )}
+        <img src={Apartment} onLoad={handleImageLoaded} alt="apartment" />
       </div>
       <div className={'flex-1 ml-2'}>
         <p>{name}</p>
